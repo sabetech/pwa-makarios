a3290e1264f12bce433fef92a0e5d5269af872db
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-10-08 13:21:46 +0000

    redirect to set photo after login if img is not set

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { IUserManager, User } from "../interfaces/ServerResponse"
+import { IUserManager } from "../interfaces/ServerResponse"
 import * as storageKeys from '../constants/StorageKeys'
+import { TUser } from '../types/user';
 
 export const UserContext = React.createContext<IUserManager>(null as any)
 interface Props {
@@ -8,9 +9,9 @@ interface Props {
 }
 
 const UserProvider: React.FC<Props> = ({ children }) => {
-    const [user, setUser] = React.useState<User>(() => JSON.parse(localStorage.getItem(storageKeys.USER) as string) as User || null as any)
+    const [user, setUser] = React.useState<TUser>(() => JSON.parse(localStorage.getItem(storageKeys.USER) as string) as TUser || null as any)
 
-    const storeUser = (user: User) => {
+    const storeUser = (user: TUser) => {
       setUser(user)
     }
 
