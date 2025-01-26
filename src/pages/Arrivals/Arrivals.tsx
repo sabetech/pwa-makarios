1801640a64aa1b5d9e13afd53d7dbc467e3edb21
Eben <bonuzx1@gmail.com>
2025-01-26 12:27:45 +0000

    change

--- src/pages/Arrivals/Arrivals.tsx
+++ src/pages/Arrivals/Arrivals.tsx
@@ -1,84 +1,45 @@
-import { useRef, useCallback } from "react";
 import MyNavBar from "../../components/NavBar";
-import { NoticeBar, Steps, Space, Form, DatePicker, Button } from "antd-mobile";
-import {InformationCircleOutline, CheckCircleFill, ClockCircleFill, CameraOutline, UploadOutline } from 'antd-mobile-icons'
-import { FaBusAlt } from "react-icons/fa";
-// import { TbCapture } from "react-icons/tb";
+import {
+  NoticeBar,
+  Button,
+  List
+} from "antd-mobile";
+import { InformationCircleOutline } from "antd-mobile-icons";
+import { useNavigate } from "react-router-dom";
+import { useAuthUser } from "../../hooks/AuthHooks";
+import { useGetBacentas } from "../../hooks/BacentaHooks";
 
-import Webcam from "react-webcam";
-
-const { Step } = Steps
 const Arrivals = () => {
-    const [form] = Form.useForm()
+  const { data: bacentas, isLoading } = useGetBacentas();
+  console.log(bacentas);
+  const loggedInUser = useAuthUser()
+  const navigate = useNavigate();
+  const user = loggedInUser()
 
-    const onSubmit = () => {
-        form.submit()
-    }
+  return (
+    <>
+      <MyNavBar prevPage="dashboard" currentPage="Arrivals" rightNode={<Button color={"default"} onClick={() => navigate("/arrivals/new")}>Add New</Button>} />
+      <NoticeBar
+        content="You bus on Fridays and Sundays."
+        color="alert"
+        icon={<InformationCircleOutline />}
+      />
 
-    const webcamRef = useRef<Webcam>(null);
-    const capture = useCallback(() => {
-        if (webcamRef.current) {
-            const imageSrc = webcamRef.current.getScreenshot();
-
-            console.log("imageSrc", imageSrc);
+      <List header={`${user.name}'s Arrival Updates`} style={{ '--header-font-size': '20px' }}>
+        {isLoading ? <List.Item>Loading...</List.Item> :
+          bacentas && bacentas.length > 0 && bacentas.map((bacenta) => (
+            <List.Item
+              key={bacenta.id}
+              prefix={bacenta.id}
+              description={bacenta.name}
+            >
+              {bacenta.name}
+            </List.Item>
+          ))
         }
-    },
-    [webcamRef]
+      </List>
+    </>
   );
-
-    return (
-        <>
-            <MyNavBar prevPage="dashboard" currentPage="Arrivals" />
-            <NoticeBar content='You bus on Fridays and Sundays.' color='alert' icon={ <InformationCircleOutline /> } />
-            
-            <Steps current={0} 
-                style={{
-                    '--title-font-size': '17px',
-                    '--description-font-size': '15px',
-                    '--indicator-margin-right': '12px',
-                    '--icon-size': '22px',
-                }}
-            >
-                <Step title='Pre-Mobilization' description='Timer. 00:02:00' icon={<ClockCircleFill />} />
-                <Step title='On Our Way' description='Timer: 01:00:00' icon={<FaBusAlt />}  />
-                <Step title='Arrived' description='11:15:00 AM' icon={<CheckCircleFill />} />
-            </Steps>
-
-            <Space block direction='vertical' style={{width: '100%'}}>
-            <Form
-                form={form}
-                footer={
-                    <Button block color='primary' onClick={onSubmit} size='large'>
-                      Submit
-                    </Button>
-                  }
-            >
-                
-                <Space style={{display: 'flex', alignContent: 'center', justifyContent: 'center', margin: '30px'}}>
-                    <Webcam 
-                        videoConstraints={{
-                            facingMode: "user",
-                            height:320
-                        }}
-                        ref={webcamRef}
-                        screenshotFormat="image/jpeg"
-                        audio={false}
-                        width={'90%'}
-                        style={{borderRadius: 40, marginRight: '5%', marginLeft: '5%'}}
-                    />
-                </Space>
-                <Space block style={{justifyContent: 'center'}}>
-                    <Button color='primary' fill='outline' style={{margin: '20px'}} size="large" onClick={capture}>
-                        <CameraOutline /> Take A Premobilization Photo
-                    </Button>
-                </Space>
-                <DatePicker />
-            </Form>
-
-            </Space>
-            
-        </>
-    )
-}
+};
 
 export default Arrivals;
