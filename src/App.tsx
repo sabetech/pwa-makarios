f144ea3a923ef660ab130b6c5ebd094cb0997f3a
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2025-02-02 16:52:49 +0000

    show members of region

--- src/App.tsx
+++ src/App.tsx
@@ -73,6 +73,7 @@ function App() {
                     
                     <Route path='/dashboard/streams/:stream_id/regions' element={<RegionIndex />} />
                     <Route path='/dashboard/regions/:id' element={<Region />}/>
+                    <Route path='/dashboard/regions/:id/members' element={<Members />} />
 
 
                     <Route path='/dashboard/zones' element={<ZoneIndex />}/>
